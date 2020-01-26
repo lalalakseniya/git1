336f16bc7743397506853fe4a1531f858a36dc50
Kseniya <alena.geraschencko@yandex.ru>
2020-01-26 16:32:27 +0300

    product №4

---
+++ src/zadanie4.java
@@ -0,0 +1,25 @@
+public class zadanie4 {
+    public static void main(String [] args){
+        int A=12;
+        int B=31;
+        int C= 55;
+        int D=100;
+        if(A % 2==0){
+            System.out.println("True");
+        }
+        else System.out.println("False");
+        if (B % 2==0){
+            System.out.println("True");
+        }
+        else System.out.println("False");
+        if (C % 2==0){
+            System.out.println("True");
+        }
+        else System.out.println("False");
+        if (D % 2==0){
+            System.out.println("True");
+        }
+        else System.out.println("False");
+        
+    }
+}
